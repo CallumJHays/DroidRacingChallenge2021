17d4a1ed7555e662df0486d7db534af5e5c19d92
CallumJHays <callumjhays@gmail.com>
2021-04-13 19:41:56 +1000

    write a niceish lil wrapper

--- demo_foc.py
+++ demo_foc.py
@@ -1,13 +1,34 @@
 from serial import Serial
 import pyvesc
+from pyvesc import GetRotorPosition, GetValues, SetCurrent, SetCurrentBrake, SetDutyCycle, SetPosition, SetRotorPositionMode, SetRPM
+from typing import Any
 
-# https://github.com/UofSSpaceTeam/roveberrypy/blob/a0bf9ada77ad0daf93c25ff917d3043f9da4cb66/roverprocess/USBServer.py
-with Serial('/dev/ttyACM0', 115200) as ser:
-    req = pyvesc.GetValues
-    ser.write(pyvesc.encode_request(req))
-    in_buf = b''
-    while ser.in_waiting > 0:
-        in_buf += ser.read(ser.in_waiting)
-    res, consumed = pyvesc.decode(in_buf)
-    print()
-    print(res)
+
+class MiniFocPlus(Serial):
+    # https://github.com/UofSSpaceTeam/roveberrypy/blob/a0bf9ada77ad0daf93c25ff917d3043f9da4cb66/roverprocess/USBServer.py
+
+    def __init__(self, port: str = '/dev/ttyACM0'):
+        super().__init__(port, 115200)
+        self.buf = b''
+
+    def command(self, cmd: Any):
+        if cmd is pyvesc.GetRotorPosition or pyvesc.GetValues:
+            fn = pyvesc.encode_request
+        else:
+            fn = pyvesc.encode
+        self.write(fn(cmd))
+        while True:
+            more = self.read_all()
+            if more:
+                self.buf += more
+                res, consumed = pyvesc.decode(self.buf)
+                if res:
+                    self.buf = self.buf[consumed:]
+                    return res
+
+
+with MiniFocPlus() as foc:
+    cmd = SetRPM()
+    vals = foc.command(SetRPM())
+    # pos = foc.get_rotor_pos()
+    print(pos)
